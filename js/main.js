b38fc92431d34b96991f9824566dc32cc47b714d
henry1479 <fokys.81@mail.ru>
2021-03-10 11:26:49 +0300

    lesson-4

--- js/main.js
+++ js/main.js
@@ -5,16 +5,19 @@ let brandButton = document.getElementById('brandButton');
 let overlayWrapper = document.getElementById('overlayWrapper');
 let buttonIcon = document.getElementById('buttonIcon');
 
-headerButton.addEventListener('click', function() {
-	overlayWrapper.classList.toggle('display-flex');
-	if(overlayWrapper.classList.contains('display-flex')) {
-		buttonIcon.style.fill = 'var(--dark-pink)';
+headerButton.addEventListener('click', function () {
+	overlayWrapper.classList.toggle('active');
+	if (overlayWrapper.classList.contains('active')) {
+		buttonIcon.style.fill = '$dark-pink';
+		overlayWrapper.style.display = 'block';
 	} else {
-		buttonIcon.style.fill = 'var(--white)';
+		buttonIcon.style.fill = '$white';
+		overlayWrapper.style.display = 'none';
 	}
 })
 
-brandButton.addEventListener('click', function() {
-	overlayWrapper.classList.remove('display-flex');
-	buttonIcon.style.fill = 'var(--white)';
+brandButton.addEventListener('click', function () {
+	overlayWrapper.classList.remove('active');
+	buttonIcon.style.fill = '$white';
+	overlayWrapper.style.display = 'none';
 })
